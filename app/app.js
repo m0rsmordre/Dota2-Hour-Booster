8c8e72cc14c4c8170150fa67911a9277599fcf51
m0rsmordre <42314954+m0rsmordre@users.noreply.github.com>
2020-08-14 17:25:52 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,7 @@
-const app = require("express")();
+
 require('dotenv').config();
 let port = process.env.port || 3000;
 
-app.get('/', (req,res) => {
-	res.send("sa");
-})
 
 app.listen(port, () =>{
 	console.log("Server ayakta");
@@ -26,7 +23,7 @@ try {
 }
 
 
-//const config = require('./Settings/config.js');
+
 const Steam = require('./steam.js');
 const infolog = require('./infolog.js');
 const method = require('./methods');
@@ -51,14 +48,14 @@ const LogOnOptionsMANUAL = {
     method.check();
 }*/
 
-// APP START
 
-// If auto generate method is used use options for login auto
+
+
 if(method.AutoGenerateLoginCodes())
     {
     client.logOn(LogOnOptionsAUTO);
 }
-// If auto generate method is disabled use options for login manual
+
 if(!method.AutoGenerateLoginCodes())
 	{
     client.logOn(LogOnOptionsMANUAL);
@@ -70,15 +67,9 @@ client.on('loggedOn', () => {
 	client.setPersona(SteamUser.Steam.EPersonaState.LookingToTrade,);
 	infolog.correct(` LOGIN |: User is logined to steam and script is ready to set idling game...`);
 	// Setting game to idle for
-	client.gamesPlayed(process.env.GameID,true);
+	client.gamesPlayed({ game_id: process.env.GameID});
 	infolog.info(`| [IDLE] | GAMES |: Bot started idling for ${process.env.GameID}`);
 });
 
 
-// Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
 
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com 
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
-
- */
