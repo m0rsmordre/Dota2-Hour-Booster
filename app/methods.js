90daa3452b027edd53b6722f94bb9dafac867b54
m0rsmordre <42314954+m0rsmordre@users.noreply.github.com>
2020-08-15 18:06:19 +0300

    Update methods.js

--- app/methods.js
+++ app/methods.js
@@ -1,12 +1,4 @@
-// Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
 
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
-
- */
-
-// Checking if required modules are properly installed
 
 try {
     // Checking if module colors is installed
@@ -38,29 +30,8 @@ t = module.exports = {
         return config.UpdateNotif_Enable == true;
     },
 
-    // Method for checking for new version
-	check: function() {
-        const request = require('request');
-        var options = {
-            url: 'https://raw.githubusercontent.com/Refloow/Steam-Games-Idle/master/package.json',
-            method: 'GET',
-        };
-        function look(error, JSONresponse, body) {
-            var page = JSON.parse(body)
-            const v = package.version;
-            if(page.version != v)
-                console.log(`| [GitHub] | VERSION |:  ${'New update available for '+package.name+ ' v'+page.version.green+'! You\'re currently only running version '+v.yellow+''}\n${`| [GitHub] | VERSION |: Go to https://github.com/Refloow/Steam-Games-Idle to update now!`}\n\n`)
-            else 
-                console.log(`| [GitHub] | VERSION |: You're running the latest version of Steam-Games-Idle (v${v.green})\n\n`)
-        }
-        request(options, look)
+    
+        
     }
 }
 
-// Steam-Games-Idle - Bot built by Refloow (-MajokingGames)
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
-
- */
